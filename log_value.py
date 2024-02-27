54221ad1b8d4ccd34fa3e523e8fe370a7e583dca
zameerzz <zameer.ou@gmail.com>
2024-02-27 14:56:13 +0100

    compute log value

---
+++ log_value.py
@@ -0,0 +1,10 @@
+import math
+
+
+def compute_log():
+    log_value = math.log(77.8 * 3.1415, 10)  # math.log(x,Base) formula
+    final_value = math.sqrt(log_value)
+    print(f"log value : {final_value}")
+
+
+compute_log()
